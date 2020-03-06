a171e9f69834dd398a09036cd1a66c94ead5124d
kayla802 <57269429+kayla802@users.noreply.github.com>
2020-03-06 14:06:37 -0600

    powers completed

---
+++ CFS1Homework/Powers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CFS1Homework
+{
+    class Powers
+    {
+        static void Main(string[] args)
+        {
+            long powerNumber = 1;
+            for (int i =0; i < 50; i++)
+            {
+                Console.WriteLine(powerNumber *= 2);
+            }
+        }
+    }
+}
